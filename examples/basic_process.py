d25fb2b46b92677e78ba93ac5bb5f37e14b27b99
bickrosss <madinatekeeva2006@gmail.com>
2026-01-03 22:43:05 +0300

    Create basic_process.py

---
+++ examples/basic_process.py
@@ -0,0 +1,9 @@
+from multiprocessing import Process
+
+def func():
+    print("Hello from child Process")
+
+if __name__ == "__main__":
+    print("Hello from main Process")
+    proc = Process(target=func)
+    proc.start()
